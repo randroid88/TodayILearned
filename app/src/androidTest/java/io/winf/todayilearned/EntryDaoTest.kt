724d0fc21ca78187f61a2cbccc42f92a421b0db1
Randy Lutcavich <rlutcavich@gmail.com>
2018-12-28 00:15:19 -0800

    - Added instrumented test for EntryDao - Using OneTimeObserver util for help testing with LiveData: Credit Alessandro Diaferia from https://hackernoon.com/testing-livedata-on-android-d717c241efb1

---
+++ app/src/androidTest/java/io/winf/todayilearned/EntryDaoTest.kt
@@ -0,0 +1,167 @@
+package io.winf.todayilearned
+
+import android.content.Context
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import androidx.lifecycle.LiveData
+import androidx.room.Room
+import androidx.test.core.app.ApplicationProvider
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import io.winf.todayilearned.util.OneTimeObserver
+import org.junit.After
+
+import org.junit.Test
+import org.junit.runner.RunWith
+
+import org.junit.Assert.*
+import org.junit.Before
+import org.junit.Rule
+import java.io.IOException
+import java.nio.charset.Charset
+import java.util.*
+
+@RunWith(AndroidJUnit4::class)
+class EntryDaoTest {
+
+    @Rule
+    @JvmField
+    var instantTaskExecutorRule = InstantTaskExecutorRule()
+
+    private lateinit var entryDao: EntryDao
+    private lateinit var db: EntryRoomDatabase
+
+    private lateinit var expectedEntries: List<Entry>
+    private lateinit var actualEntries: List<Entry>
+
+    @Before
+    fun createDb() {
+        val context = ApplicationProvider.getApplicationContext<Context>()
+        db = Room.inMemoryDatabaseBuilder(context, EntryRoomDatabase::class.java)
+                .allowMainThreadQueries()
+                .build()
+        entryDao = db.entryDao()
+    }
+
+    @After
+    @Throws(IOException::class)
+    fun closeDb() {
+        db.close()
+    }
+
+    @Test
+    @Throws(Exception::class)
+    fun noEntries() {
+        thenNoEntriesExist()
+    }
+
+    @Test
+    @Throws(Exception::class)
+    fun insertEntry() {
+        givenEntries(1)
+
+        whenEntriesInserted()
+
+        thenEntriesExist()
+    }
+
+    @Test
+    @Throws(Exception::class)
+    fun insertAndGetEntriesInOrder() {
+        givenEntries(3)
+
+        whenEntriesInserted()
+
+        thenEntriesExist()
+        thenEntriesSizeIs(3)
+        thenEntriesIdsIncrement()
+        thenEntriesTextIsAsSet()
+        thenEntriesDateTimeIsAsSet()
+    }
+
+    @Test
+    @Throws(Exception::class)
+    fun deleteAllEntries() {
+        givenEntriesInserted()
+
+        whenAllEntriesDeleted()
+
+        thenNoEntriesExist()
+    }
+
+    private fun givenEntriesInserted() {
+        givenEntries(3)
+
+        whenEntriesInserted()
+
+        thenEntriesExist()
+    }
+
+    // Helper Kotlin extension for testing LiveData
+    fun <T> LiveData<T>.observeOnce(onChangeHandler: (T) -> Unit) {
+        val observer = OneTimeObserver(handler = onChangeHandler)
+        observe(observer, observer)
+    }
+
+    private fun givenEntries(numberOfEntries: Int) {
+        var firstEntryDateTime = givenAnyLong() - numberOfEntries
+        expectedEntries = List(numberOfEntries) {
+            Entry(givenAnyString(), firstEntryDateTime++)
+        }
+    }
+
+    private fun givenAnyLong(): Long {
+        return Random().nextLong()
+    }
+
+    private fun givenAnyString(): String {
+        val array = ByteArray(7)
+        Random().nextBytes(array)
+        return String(array, Charset.forName("UTF-8"))
+    }
+
+    private fun whenEntriesInserted() {
+        for (entry in this.expectedEntries)
+            entryDao.insert(entry)
+    }
+
+    private fun whenAllEntriesDeleted() {
+        entryDao.deleteAll()
+    }
+
+    private fun thenNoEntriesExist() {
+        entryDao.orderedEntries.observeOnce {
+            assertEquals(0, it.size)
+        }
+    }
+
+    private fun thenEntriesExist() {
+        entryDao.orderedEntries.observeOnce {
+            assertNotEquals(0, it.size)
+            actualEntries = it
+        }
+    }
+
+    private fun thenEntriesSizeIs(expectedSize: Int) {
+        assertEquals(expectedSize, actualEntries.size)
+    }
+
+    private fun thenEntriesTextIsAsSet() {
+        for (i in 0 until expectedEntries.size) {
+            assertEquals(expectedEntries[i].entryText, actualEntries[i].entryText)
+        }
+    }
+
+    private fun thenEntriesDateTimeIsAsSet() {
+        for (i in 0 until expectedEntries.size) {
+            assertEquals(expectedEntries[i].entryDateTime, actualEntries[i].entryDateTime)
+        }
+    }
+
+    private fun thenEntriesIdsIncrement() {
+        for (i in 0 until expectedEntries.size) {
+            assertEquals(i + 1, actualEntries[i].id)
+        }
+    }
+}
+
+
+
