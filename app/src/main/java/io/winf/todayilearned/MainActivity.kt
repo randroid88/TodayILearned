1e60f1d79eebd48df8f0d2ed8b7e494406e20450
Randy Lutcavich <rlutcavich@gmail.com>
2018-12-28 17:44:17 -0800

    - Using Android Navigation Architecture Component - Changed the main activity to use the NavHostFragment - Setup HomeFragment to be the starting destination in the nav graph

--- app/src/main/java/io/winf/todayilearned/MainActivity.kt
+++ app/src/main/java/io/winf/todayilearned/MainActivity.kt
@@ -2,11 +2,14 @@ package io.winf.todayilearned
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.fragment.NavHostFragment
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment? ?: return
     }
 }
