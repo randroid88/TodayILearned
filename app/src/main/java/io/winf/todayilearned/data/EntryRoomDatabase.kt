fd5c3fd52bbe64f8ae057438d73ff7209c8bf6df
Randy Lutcavich <rlutcavich@gmail.com>
2019-01-01 20:52:13 -0800

    - Removed default entries from the db

--- app/src/main/java/io/winf/todayilearned/data/EntryRoomDatabase.kt
+++ app/src/main/java/io/winf/todayilearned/data/EntryRoomDatabase.kt
@@ -1,11 +1,9 @@
 package io.winf.todayilearned.data
 
 import android.content.Context
-import android.os.AsyncTask
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
-import androidx.sqlite.db.SupportSQLiteDatabase
 
 @Database(entities = [Entry::class], version = 1)
 abstract class EntryRoomDatabase : RoomDatabase() {
@@ -24,41 +22,11 @@ abstract class EntryRoomDatabase : RoomDatabase() {
                         INSTANCE = Room.databaseBuilder(context.applicationContext,
                                 EntryRoomDatabase::class.java, "entry_database")
                                 .fallbackToDestructiveMigration()
-                                .addCallback(roomDatabaseCallback)
                                 .build()
                     }
                 }
             }
             return INSTANCE as EntryRoomDatabase
         }
-
-        private val roomDatabaseCallback = object : RoomDatabase.Callback() {
-
-            override fun onOpen(db: SupportSQLiteDatabase) {
-                super.onOpen(db)
-                // ToDo: Remove temporary dummy data
-                PopulateDbAsync(INSTANCE!!).execute()
-            }
-        }
-    }
-
-
-}
-
-private class PopulateDbAsync internal constructor(db: EntryRoomDatabase) : AsyncTask<Void, Void, Void>() {
-
-    private val dao: EntryDao
-
-    init {
-        dao = db.entryDao()
-    }
-
-    override fun doInBackground(vararg params: Void): Void? {
-        dao.deleteAll()
-        var entry = Entry("Today I learned how to store text entries in a Room database.", System.currentTimeMillis())
-        dao.insert(entry)
-        entry = Entry("Today I learned why the Kotlin Annotation Processor is needed.", System.currentTimeMillis())
-        dao.insert(entry)
-        return null
     }
 }
